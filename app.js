5f7bc904e0ed1c557b7494307d3d9ff86cb597fd
Cooper Viktor <im.cooperviktor@gmail.com>
2019-05-25 13:16:14 +1000

    adding local storage

--- app.js
+++ app.js
@@ -67,6 +67,35 @@ class UI {
 }
 //! Storage CLass
 
+class Store {
+  static getBooks() {
+    let books;
+    if(localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+    return books;
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static removeBook(isbn) {
+    const books = Store.getBooks();
+
+    books.forEach((book, index) => {
+      if(book.isbn === isbn) {
+        books.splice(index, 1);
+      }
+    });
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
+
 //! Event - Display a book
 
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
@@ -91,6 +120,9 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     // Add book to list
     UI.addBookToList(book);
   
+    // Show Success Message
+    UI.showAlert('Book Added', 'success');
+
     // clear fields
     UI.clearFields();
   }
@@ -99,4 +131,6 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
 //! Event - Remove a Book
 document.querySelector('#book-list').addEventListener('click', (e) => {
   UI.deleteBook(e.target);
+  // delete book
+  UI.showAlert('Book Deleted', 'success');
 })
